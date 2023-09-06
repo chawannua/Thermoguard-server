cabb9b59e1093f77b43f5fd7b09a6373b72866e3
chawannua <55906160+chawannua@users.noreply.github.com>
2023-09-06 18:07:55 +0700

    Update

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-  const express = require('express');
-  const path = require('path');
-  const app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-  const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-  app.use(express.static(path.join(__dirname, 'Thermoguard-server')));
+// Serve static files from the "Thermoguard-server" directory
+app.use(express.static(path.join(__dirname, 'Thermoguard-server')));
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+// Define a route handler for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Yes,Finally it fukcing work'); // Send a response when the root URL is accessed
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
